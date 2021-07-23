33912193ff2cf9ff9acc58266d9984d8196ea8dd
Slavko_V <slavko.vuletic92@gmail.com>
2021-07-23 12:25:05 +0200

    Yup first module export import set for logger

---
+++ app.js
@@ -0,0 +1,57 @@
+const { logMSG, infoMSG, warnMSG, errorMSG } = require("./PUBLIC/test_Logger/test_log")
+
+// Testing the log functions
+errorMSG("FFFFFFFFFFFFFFFFFFFFFFFFFF");
+logMSG("Hello from app.js");
+infoMSG("Info message from app.js");
+warnMSG("Basic warning about app.js");
+errorMSG("Yo yo shit is going down in the app.js");
+
+
+// Express
+require("dotenv").config();
+const compression = require("compression");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const APP_PROTOCOL = process.env.APP_PROTOCOL;
+const APP_HOST = process.env.APP_HOST;
+const APP_PORT = process.env.APP_PORT;
+const APP_FOLDER = process.env.APP_FOLDER;
+let APP_URL = APP_PROTOCOL + "://" + APP_HOST; 
+if ((APP_PORT == "") || (APP_FOLDER == "")){
+
+    if (APP_PORT != ""){
+        APP_URL = APP_URL + ":" + APP_PORT;
+    } 
+    
+    if (APP_FOLDER != ""){
+        APP_URL = APP_URL + "/" + APP_FOLDER;
+    }
+
+} else {
+
+}
+
+const STATIC = path.resolve(__dirname, "PUBLIC");
+const INDEX = path.resolve(STATIC, "index.html");
+
+
+const app = express();
+app.use(bodyParser.json());
+
+// compress all responses
+app.use(compression());
+// Static content
+app.use(express.static(STATIC));
+
+// All GET request handled by INDEX file
+app.get("*", function (req, res) {
+    res.sendFile(INDEX);
+});
+
+// Start server
+app.listen(APP_PORT, function () {
+  console.log("Server up and running on " + APP_URL);
+});
