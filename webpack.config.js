6a12d789e079afbf7f5d7e991cd4f765b467cf63
Slavko_V <slavko.vuletic92@gmail.com>
2021-07-24 07:13:23 +0200

    yea

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,6 @@
-const path = require("path");
-require("dotenv").config();
-const {
-  logMSG,
-  infoMSG,
-  warnMSG,
-  errorMSG,
-} = require("./src/helpers/loging_function");
+const path = require("path")
+require("dotenv").config()
+const { logMSG, infoMSG, warnMSG, errorMSG } = require("./src/helpers/loging_function")
 
 const config = {
   production: {
@@ -33,21 +28,21 @@ const config = {
       filename: "v_logger.js",
     },
   },
-};
+}
 
 if (typeof process.env.NODE_ENV !== "undefined") {
   infoMSG("FOUND process.env.NODE_ENV === " + process.env.NODE_ENV);
   if (process.env.NODE_ENV === "production") {
-    module.exports = config.production;
+    module.exports = config.production
   } else if (process.env.NODE_ENV === "development") {
-    module.exports = config.development;
+    module.exports = config.development
   } else if (process.env.NODE_ENV === "none") {
     module.exports = config.none;
   } else {
-    module.exports = config.production;
+    module.exports = config.production
   }
 } else {
   // module.exports = require("./cjs/react.development.js");
   errorMSG("process.env.NODE_ENV === UNDEFINED || WILL SET TO PRODUCTION");
-  module.exports = config.production;
+  module.exports = config.production
 }
