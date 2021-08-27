469266a41a75d5d713a3bf3db6718708aa1f1478
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-08-27 02:43:27 +0200

    preparing command generation

---
+++ -v-/generate_commands.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ 
+# Declare a string array with type
+declare -a StringArray=("users" "versions" "uuid" "graphics" "battery" "processes" "services" "disk_layout" "fs_stats" "disk_io" "open_files" "audio" "net_stats" "net_conn" "net_def_gateway" "net_def_interface" "net_interfaces" )
+ 
+# Read the array values with space
+for val in "${StringArray[@]}"; do
+  echo 'sys_'$val
+done
