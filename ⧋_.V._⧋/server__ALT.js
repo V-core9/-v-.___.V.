882329cd35dd37e703c374c2e7c61e91262b88ae
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-10-15 19:12:09 +0200

    env files added and configs folder + jshint and fix in server_alt

--- ⧋_.V._⧋/server__ALT.js
+++ ⧋_.V._⧋/server__ALT.js
@@ -1,30 +1,11 @@
-
-
-// Testing the log functions
-console.log("Hello from app.js");
-console.log("Info message from app.js");
-console.log("Basic warning about app.js");
-console.log("Yo yo shit is going down in the app.js");
-
-if (typeof process.env.NODE_ENV !== "undefined") {
-  if (process.env.NODE_ENV === "production") {
-    console.log("PRODUCTION MODE");
-  } else if (process.env.NODE_ENV === "development") {
-    console.log("DEV MODE");
-  } else {
-    console.log("process.env.NODE_ENV must be present! Available modes are ['production', 'development']");
-  }
-} else {
-  console.log("process.env.NODE_ENV must be present! Available modes are ['production', 'development']");
-}
-// Express
+// Rquired things before we run___
 require("dotenv").config();
 const compression = require("compression");
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-// IsOnline NPM require
 const isOnline = require("is-online");
+
 //check if is online
 (async () => {
   console.log(await isOnline());
@@ -33,6 +14,7 @@ const isOnline = require("is-online");
 })();
 // END IsOnline
 
+//> Basic test if in dev mode
 if (typeof process.env.NODE_ENV !== "undefined") {
   if (process.env.NODE_ENV === "production") {
     console.log("PRODUCTION MODE");
@@ -44,30 +26,28 @@ if (typeof process.env.NODE_ENV !== "undefined") {
 } else {
   console.log("process.env.NODE_ENV must be present! Available modes are ['production', 'development']");
 }
+//--------------------
 
+const STATIC = path.resolve(__dirname, "PUBLIC");
+const INDEX = path.resolve(STATIC, "index.html");
 const AppPROTOCOL = (typeof process.env.APP_PROTOCOL !== "undefined") ? process.env.APP_PROTOCOL : "http";
 const AppHOST = (typeof process.env.APP_HOST !== "undefined") ? process.env.APP_HOST : "localhost";
 const AppPORT = (typeof process.env.APP_PORT !== "undefined") ? process.env.APP_PORT : "3200";
 const AppFOLDER = (typeof process.env.APP_FOLDER !== "undefined") ? process.env.APP_FOLDER : "";
 let AppURL = `${AppPROTOCOL}://${AppHOST}:${AppPORT}/${AppFOLDER}`;
 
-const STATIC = path.resolve(__dirname, "PUBLIC");
-const INDEX = path.resolve(STATIC, "index.html");
 
 
 const app = express();
 app.use(bodyParser.json());
-
 // compress all responses
 app.use(compression());
 // Static content
 app.use(express.static(STATIC));
-
 // All GET request handled by INDEX file
 app.get("*", function (req, res) {
   res.sendFile(INDEX);
 });
-
 // Start server
 app.listen(AppPORT, function () {
   console.log("Server up and running on " + AppURL);
---
+++ ⧋_.V._⧋/vApp/index.js
@@ -0,0 +1,143 @@
+require("dotenv").config();
+
+const fs = require('fs')
+
+const vApp = {
+  data: {
+    totalPageViews: 0,
+    totalUsersVisit: 0,
+  },
+  vClientApp: {
+    styles: [
+      {
+        type: "name",
+        style: "glide_slider"
+      },
+      {
+        type: "url",
+        style: "https://randomweburl.123/$33221/41?213"
+      },
+      {
+        type: "code",
+        style() {
+          return `
+        * {
+          font-family: monospace;
+          padding: 0;
+          margin: 0;
+          font-size: 16px;
+        }
+
+        html, body {
+          background: $101525;
+          color: white;
+        }
+      `
+        }
+      }
+    ]
+  },
+  sitemap: {
+    xml_page: null,
+    path: "/sitemap_index.xml"
+  },
+  compression: require("compression"),
+  express: require("express"),
+  bodyParser: require("body-parser"),
+  path: require("path"),
+  isOnline: require("is-online"),
+  app: null,
+  helpers: {
+    printInlineScript : (codeToPrint = null) => {
+      if (codeToPrint !== null) {
+        try {
+          return `<script>${codeToPrint}</script>`;
+        } catch (err) {
+          return err;
+        };
+      };
+      return false;
+    }
+  },
+  requestTime(req, res, next) {
+    req.requestTime = Date.now();
+    next();
+  },
+  userAgent(req, res, next) {
+    req.userAgent = req.headers['user-agent'];
+    next();
+  },
+  myLogger(req, res, next) {
+    //console.log(`Host: ${req.headers.host}  \nUser - agent:   ${req.userAgent} \nTIMEOF: ${Date.now()}`);
+    next();
+  },
+  pageViewCounter(req, res, next) {
+    vApp.data.totalPageViews++;
+    next();
+  },
+
+  init($_PORT = 3000) {
+
+    vApp.sitemap.xml_page = fs.readFileSync('./PUBLIC/sitemap_index.xml', { encoding: 'utf8', flag: 'r' })
+
+    console.log(vApp.sitemap);
+    
+    
+    vApp.app = vApp.express();
+    vApp.app.use(vApp.requestTime);
+    vApp.app.use(vApp.userAgent);
+    vApp.app.use(vApp.myLogger);
+    vApp.app.use(vApp.pageViewCounter);
+    
+    
+    
+    vApp.app.get('/sitemap_index.xml', (req, res) => {
+    
+      res.type('application/xml');
+      res.send(vApp.sitemap.xml_page);
+    });
+    
+    
+    vApp.app.get('/*', (req, res) => {
+    
+      var responseText = `${vApp.helpers.printInlineScript(JSON.stringify(vApp.vClientApp))}<h2>GET YEAAA World!</h2> 
+              <p> Requested at: <strong> ${req.requestTime} </strong></p>
+              <p>User Agent: <strong>${req.userAgent} </strong></p>
+              <p>app : <strong>${req.app}</strong></p>
+              <p>baseUrl : <strong>${req.baseUrl}</strong></p>
+              <p>body : <strong>${req.body}</strong></p>
+              <p>cookies : <strong>${req.cookies}</strong></p>
+              <p>fresh : <strong>${req.fresh}</strong></p>
+              <p>host : <strong>${req.host}</strong></p>
+              <p>hostname : <strong>${req.hostname}</strong></p>
+              <p>ip : <strong>${req.ip}</strong></p>
+              <p>ips : <strong>${req.ips}</strong></p>
+              <p>method : <strong>${req.method}</strong></p>
+              <p>originalUrl : <strong>${req.originalUrl}</strong></p>
+              <p>params : <strong>${req.params}</strong></p>
+              <p>path : <strong>${req.path}</strong></p>
+              <p>protocol : <strong>${req.protocol}</strong></p>
+              <p>query : <strong>${req.query}</strong></p>
+              <p>route : <strong>${req.route}</strong></p>
+              <p>secure : <strong>${req.secure}</strong></p>
+              <p>signedCookies : <strong>${req.signedCookies}</strong></p>
+              <p>stale : <strong>${req.stale}</strong></p>
+              <p>subdomains : <strong>${req.subdomains}</strong></p>
+              <p>xhr : <strong>${req.xhr}</strong></p>
+              <p>URL: <strong> ${req.path} </strong></p>`;
+    
+      res.send(responseText);
+    });
+    
+    setInterval(() => {
+      console.log(vApp.data.totalPageViews)
+    }, 1000);
+    
+    
+    
+    vApp.app.listen($_PORT);
+    
+  }
+}
+
+module.exports = vApp;
